bb1a5ca6af14c36574ea35c257ff1e7de04c683f
Annie He <anniehe.yt@gmail.com>
2016-03-15 22:43:09 -0700

    added alternative solution to sortab

--- sortab.py
+++ sortab.py
@@ -43,6 +43,31 @@ def sort_ab(a, b):
 
     # Runtime is O(n) where n = len(a) + len(b)
 
+    # # ALTERNATIVE SOLUTION
+    # out = []
+
+    # ia = 0  # index into list a
+    # ib = 0  # index into list b
+
+    # while ia < len(a) and ib < len(b):
+    #     # Check current items in both lists:
+    #     # if a < b, add a and increase ia
+    #     # else, add b and increase ib
+
+    #     if a[ia] < b[ib]:
+    #         out.append(a[ia])
+    #         ia += 1
+    #     else:
+    #         out.append(b[ib])
+    #         ib += 1
+
+    # # One list could have things remaining; add any remaining items
+    # out.extend(a[ia:])
+    # out.extend(b[ib:])
+
+    # return out
+
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
